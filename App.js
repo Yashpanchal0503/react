7a23cff3b8f3fc3b18997f66656373fb57c12828
YashPanchal0503 <yash2007panchal@gmail.com>
2026-08-15 19:09:23 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,48 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/*
-    <div id="parent">
-        <div id="child1">
-            <h1>i am h1 tag</h1>
-            <h2>i am h2 tag</h2>
-        </div> 
-        <div id="child2">
-            <h1>i am h1 tag</h1>
-            <h2>i am h2 tag</h2>
+
+// JSX is HTML/XML-like syntax
+
+// React component
+const Title = () => {
+    return (
+        <h1 id="heading">
+            namaste react from jsx
+        </h1>
+    );
+};
+
+// Function-based component
+const HeadingComponent = () => {
+    return (
+        <div id="container">
+            <Title />
+            <h1>namaste from functional component</h1>
         </div>
+    );
+};
+
+// Implicit return
+const HeadingComponent2 = () => (
+    <div id="container">
+        <h1>namaste from functional component</h1>
     </div>
-*/
+);
 
-const parent =React.createElement("div",{id:"parent"},
-    [
-        React.createElement("div",{id:"child1"},
-            [
-                React.createElement("h1",{},"i am h1 tag"),
-                React.createElement("h2",{},"i am h2 tag")
-            ]
-        ),
-        React.createElement("div",{id:"child2"},
-            [
-                React.createElement("h1",{},"i am h1 tag"),
-                React.createElement("h2",{},"i am h2 tag")
-            ]
-        )
-    ]
-)
-
-
-
-
-
-const head = React.createElement(
-    "h1",
-    {id:"heading" , abc:"xyz"},
-    "hello world from react!"
-); 
-console.log(head);// it will just give a object to us
-
-
+const HeadingComponent3 = () =>  <h1>namaste from functional component3</h1>
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+
+root.render(<HeadingComponent3 />);
